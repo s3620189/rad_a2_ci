f3f5bc8496198b4ee5dad649b4fef710aa208af9
s3620189 <s3620189@student.rmit.edu.au>
2020-05-14 04:52:20 +1000

    fix bug

--- ansible/run_ansible.sh
+++ ansible/run_ansible.sh
@@ -4,4 +4,6 @@ set +ex
 # todo genereate inventory.yml file with ec2 host
 
 # todo add any additional variables
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory.yml -e 'record_host_keys=True' -u ec2-user --private-key <ssh key> playbook.yml
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts -e 'record_host_keys=True' playbook.yml
+
+#ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook aws -i hosts -e 'record_host_keys=True' -u ec2-user --private-key ~/.ssh/id_rsa playbook.yml
