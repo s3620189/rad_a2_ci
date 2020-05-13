637056c30e65cd4552e46325ecfd7f1e1022a049
s3620189 <s3620189@student.rmit.edu.au>
2020-05-14 04:49:59 +1000

    db fix

--- infra/bastion.tf
+++ infra/bastion.tf
@@ -17,7 +17,13 @@ resource "aws_security_group" "allow_http_ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  ingress {
+    description = "connectdb from internet"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
--- infra/db.tf
+++ infra/db.tf
@@ -9,7 +9,7 @@ resource "aws_db_instance" "default" {
   password               = "db_password"
   vpc_security_group_ids = [aws_security_group.allow_http_ssh.id]
   db_subnet_group_name   = aws_db_subnet_group.data.name
-
+  skip_final_snapshot    = true
 
 }
 
