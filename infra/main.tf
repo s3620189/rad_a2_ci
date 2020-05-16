640418d82b3f1d6b38fd0f3b276a108659d659e9
s3620189 <s3620189@student.rmit.edu.au>
2020-05-17 03:37:31 +1000

    ci

--- infra/main.tf
+++ infra/main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   version    = "~> 2.23"
   region     = "us-east-1"
-  access_key = "ASIAQW7BMCTQ3CXGVS5R"
-  secret_key = "kF5NmSO+sbMqWkBUmM5QfgUz0cph7mBCG6yoswzO"
-  token      = "FwoGZXIvYXdzEG8aDAuvQuCGM/aCAjFl2yLLAVVTE1RUW9AnPVoCtbDHqoS469QkNtRno/rZ4O633jUdhN6MHqIZVVBzRgPXlfOqTQX2s/iK0LNw1tnEbGN8FrQwxzq4a+1+d1RpwbRftQmikWAYkM/tGtQw3Iwr8Q6+SGQHWnFQDt/wlT2n5Z82hhm9xjU9Pn3AlhMMnkHgBZV9XK/rZc7lZW+eVgpmh4O023iLD+x+lDIa5tJtdJOELdCzwkf4DBIVBpUZ8Jh7kgKTyJzswAKMjW69SRc2zOPJsepUHw534qkvbmN/KJDZ//UFMi3gT3ZahpdwAkPtU26xooGd9B/5w9y01wcaWCHcIxk0nEaJ5yPiCOH2df9FReU="
+  access_key = "ASIAQW7BMCTQ6KAKOH3W"
+  secret_key = "9r8lu76g6DUyZ1XTEtZeAjrGr213uxjLX0LZcRJ8"
+  token      = "FwoGZXIvYXdzEHAaDKTQKYNjJQgyK76fXSLLATckW3XvehTl3KOrpixehADRtvbRyAqFMZTOJyHVnlDmGBV/gAdtWYv1wIq/xKtBIudM8mJL0OM8B/MuBcBTB3EgpvuyLJ4YYO32KJagAjhWdDlygQNhhi0octfvMvYMiHRuStWgvw1d/PFVem43yVpqn+NdG1qw+VHG+xtXY1rgYgXasLSyd6SYcbJnZIvEw15Ut7E34wLejZp9z/NKSyrBXp9Pzd66QYl3VIggi9B8nR2FjpZIz7Md9kKMm1YY+lvVUQAW9lhYoAsAKJDv//UFMi2T5hLnPm1EPIxXScvSS+868nMF7oj3Iy/NBVBeqeCW9uv3ltN2msxQSoSSSSA="
 }
 
--- infra/s3.tf
+++ infra/s3.tf
@@ -1,13 +1,30 @@
 resource "aws_s3_bucket" "bucket" {
-  bucket = "app_bucket"
-  acl    = "public"
-
-  tags = {
-    Name        = "app_bucket"
-    Environment = "Dev"
-
+  bucket = "buckets3620189"
+  versioning {
+    enabled = true
   }
 
 }
 
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "s3620189"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "buckets3620189"
+    key            = "state/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "s3620189"
+  }
+
+
+}
 
