d8a814623ee99b34713bb165177677750d96207b
s3620189 <s3620189@student.rmit.edu.au>
2020-05-17 03:52:20 +1000

    ci

--- infra/up_load_app.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-terraform validate
-terraform fmt
-terraform plan
-terraform apply --auto-approve
-./auto_set_hosts_ip.sh
-
-ANSIBLE_HOST_KEY_CHECKING=False ansible -i hosts --private-key ~/.ssh/id_rsa -u ec2-user copy -a "src=dist/ dest=app/dist owner=root group=root mode=0777"
